accafbf245ed070a259a1c9e7ed8f0cba00eff66
NathaliaVillegas <nathalia.villegas@ucb.edu.bo>
2026-03-05 16:55:55 -0400

    Labo completo

--- labo_2/ex3.py
+++ labo_2/ex3.py
@@ -1,21 +1,46 @@
-import random
+import RPi.GPIO as GPIO
 import time
+import random
 
-while True:
-	temp = random.uniform(0, 35)
-	print(f"tempratura actual: {temp:.2f}")
-	
-	if temp<12:
-		print("calentador on")
-		print("ventitador off")
-		
-	elif temp>20:
-		print("calentador off")
-		print("ventitador on")
-	
-	else:
-	 	print("calentador off")
-		print("ventitador off")
-		
-time.sleep(3)
-	
+PIN_CALENTADOR = 26
+PIN_VENTILADOR = 19
+
+def configurar_sistema():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    
+    GPIO.setup(PIN_CALENTADOR, GPIO.OUT)
+    GPIO.setup(PIN_VENTILADOR, GPIO.OUT)
+    
+    GPIO.output(PIN_CALENTADOR, GPIO.LOW)
+    GPIO.output(PIN_VENTILADOR, GPIO.LOW)
+
+def controlar_clima(temperatura):
+    print(f"Temperatura: {temperatura}°C")
+
+    if temperatura < 12:
+        GPIO.output(PIN_CALENTADOR, GPIO.HIGH)
+        GPIO.output(PIN_VENTILADOR, GPIO.LOW)
+        
+    elif temperatura > 20:
+        GPIO.output(PIN_CALENTADOR, GPIO.LOW)
+        GPIO.output(PIN_VENTILADOR, GPIO.HIGH)
+        
+    else:
+        GPIO.output(PIN_CALENTADOR, GPIO.LOW)
+        GPIO.output(PIN_VENTILADOR, GPIO.LOW)
+
+def programa_principal():
+    configurar_sistema()
+    
+    while True:
+        temperatura_falsa = random.randint(5, 30)
+        
+        controlar_clima(temperatura_falsa)
+        time.sleep(2.0)
+
+if __name__ == '__main__':
+    try:
+        programa_principal()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
