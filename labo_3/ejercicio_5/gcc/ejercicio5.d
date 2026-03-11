bb5bb4f04f69be8c26a00c6fe51edb48723aaae1
Nathalia <nathalia.villegas@ucb.edu.bo>
2026-03-11 06:04:52 -0400

    Ejercicio 5

---
+++ labo_3/ejercicio_5/gcc/ejercicio5.d
@@ -0,0 +1,7 @@
+gcc/ejercicio5.o: ejercicio5.c \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ /home/nath/Documentos/SDKs/Tiva/TivaWare/inc/hw_memmap.h \
+ /home/nath/Documentos/SDKs/Tiva/TivaWare/driverlib/debug.h \
+ /home/nath/Documentos/SDKs/Tiva/TivaWare/driverlib/gpio.h \
+ /home/nath/Documentos/SDKs/Tiva/TivaWare/driverlib/sysctl.h
