b3346d6c41eb64722b0958853e3038f66fda77d6
LeonardoRondo <leonardorondo3@gmail.com>
2026-03-12 16:38:58 -0400

    Añadí los ejercicios cortos 1 y 3 del labo 3

---
+++ lab3pr3/gcc/lab3pr3.d
@@ -0,0 +1,5 @@
+gcc/lab3pr3.o: lab3pr3.c \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ ../../../../../inc/hw_memmap.h ../../../../../driverlib/sysctl.h \
+ ../../../../../driverlib/gpio.h
