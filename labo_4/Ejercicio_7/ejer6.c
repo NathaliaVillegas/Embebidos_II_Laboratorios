c12a02b254350c07acfc960559b5ce1e25fcb142
Julian [Callizaya] <josecallizaya16@gmail.com>
2026-03-19 20:41:14 -0400

    Ultimo cambio Ejercicio 6

--- labo_4/Ejercicio_7/ejer6.c
+++ labo_4/Ejercicio_7/ejer6.c
@@ -1,84 +1,74 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
 #include "inc/hw_ints.h"
+#include "inc/hw_gpio.h"    // <-- IMPORTANTE: Para las macros de desbloqueo
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 #include "driverlib/timer.h"
 #include "driverlib/interrupt.h"
-#include "driverlib/pin_map.h"
 
-// Definición de constantes para los retardos (Reloj a 120MHz)
-#define FS_1_SEG    120000000  // [cite: 114, 116]
-#define FS_05_SEG    60000000  // [cite: 171]
+#define PERIOD_1S   120000000
+#define PERIOD_05S   60000000
 
-// Variable global para mantener el valor actual de carga
-uint32_t ui32Periodo = FS_1_SEG;
+volatile uint32_t g_ui32Periodo = PERIOD_1S;
 
-// --- Manejador de la Interrupción del Timer 0A ---
-// [cite: 102, 128, 130]
-void timer0A_handler(void)
+void Timer0IntHandler(void)
 {
-    // Limpiar la interrupción del Timer [cite: 142]
-    TimerIntClear(TIMER0_BASE, TIMER_A);
+    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
 
-    // Leer el Switch del usuario (Puerto J, Pin 0) [cite: 134, 171]
-    // Si se presiona el switch (lectura == 0), cambiamos el intervalo
-    if (GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) == 0) 
+    // Lectura del botón USR_SW1 (PJ0)
+    if(GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) == 0)
     {
-        ui32Periodo = FS_05_SEG; // 0.5 segundos [cite: 171]
-    } 
-    else 
+        g_ui32Periodo = PERIOD_05S; // Rápido
+    }
+    else
     {
-        ui32Periodo = FS_1_SEG;  // 1.0 segundo [cite: 171]
+        g_ui32Periodo = PERIOD_1S;  // Lento
     }
 
-    // Actualizar la carga del Timer para el siguiente ciclo [cite: 120, 140]
-    TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Periodo);
+    TimerLoadSet(TIMER0_BASE, TIMER_A, g_ui32Periodo - 1);
 
-    // Alternar (Toggle) el LED PN1 [cite: 168]
-    if (GPIOPinRead(GPIO_PORTN_BASE, GPIO_PIN_1)) {
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0);
-    } else {
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, GPIO_PIN_1);
-    }
+    // Toggle LED PN1
+    uint32_t ui32LED = GPIOPinRead(GPIO_PORTN_BASE, GPIO_PIN_1);
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, ui32LED ^ GPIO_PIN_1);
 }
 
 int main(void)
 {
     uint32_t ui32SysClock;
 
-    // 1. Configurar el reloj del sistema a 120 MHz [cite: 107]
     ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | 
                                        SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480), 120000000);
 
-    // 2. Habilitar periféricos: Puerto N (LEDs), Puerto J (Switch) y Timer 0 [cite: 109, 113]
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
 
-    // Esperar a que los periféricos estén listos
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION));
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ));
 
-    // 3. Configurar PN1 como salida (LED) y PJ0 como entrada con Pull-up (Switch)
-    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_1);
+    // --- PASO CLAVE: DESBLOQUEO DEL PUERTO J ---
+    // El pin PJ0 necesita esto para poder activar el Pull-up
+    HWREG(GPIO_PORTJ_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
+    HWREG(GPIO_PORTJ_BASE + GPIO_O_CR) |= GPIO_PIN_0;
+    // -------------------------------------------
+
+    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1);
     GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_0);
     GPIOPadConfigSet(GPIO_PORTJ_BASE, GPIO_PIN_0, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 
-    // 4. Configurar el Timer 0 como periódico de 32 bits [cite: 119]
+    // Señal de vida (PN0 encendido fijo)
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
+
     TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
-    TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Periodo); // Carga inicial de 1s [cite: 120]
+    TimerLoadSet(TIMER0_BASE, TIMER_A, g_ui32Periodo - 1);
 
-    // 5. Configurar interrupciones [cite: 117, 122]
-    IntMasterEnable();                 // Habilitar interrupciones globales [cite: 122]
-    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT); // Interrupción por timeout [cite: 126]
-    IntEnable(INT_TIMER0A);            // Habilitar interrupción específica en el NVIC [cite: 124]
+    IntMasterEnable();
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    IntEnable(INT_TIMER0A);
 
-    // 6. Activar el Timer [cite: 127]
     TimerEnable(TIMER0_BASE, TIMER_A);
 
-    while(1)
-    {
-        // El procesador puede hacer otras tareas aquí [cite: 100, 166]
-    }
+    while(1) {}
 }
--- labo_4/Ejercicio_7/gcc/ejer6.d
+++ labo_4/Ejercicio_7/gcc/ejer6.d
@@ -1,9 +1,9 @@
 gcc/ejer6.o: ejer6.c /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
  /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
  /home/julian/TivaWarePath/inc/hw_memmap.h \
+ /home/julian/TivaWarePath/inc/hw_types.h \
  /home/julian/TivaWarePath/inc/hw_ints.h \
  /home/julian/TivaWarePath/driverlib/sysctl.h \
  /home/julian/TivaWarePath/driverlib/gpio.h \
  /home/julian/TivaWarePath/driverlib/timer.h \
- /home/julian/TivaWarePath/driverlib/interrupt.h \
- /home/julian/TivaWarePath/driverlib/pin_map.h
+ /home/julian/TivaWarePath/driverlib/interrupt.h
