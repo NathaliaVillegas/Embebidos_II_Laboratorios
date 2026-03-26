99e8a519bb3068d53b591b64b2a23788a143b20d
NathaliaVillegas <nathalia.villegas@ucb.edu.bo>
2026-03-26 17:11:24 -0400

    Ejercicio_3

--- labo_5/Ejercicio_3.py
+++ labo_5/Ejercicio_3.py
@@ -4,29 +4,33 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-def configuracion_pines():
-	pin = 18
-	pin_adelante = 23
-	pin_atras = 24
+pin_pwm = 18
+pin_adelante = 23
+pin_atras = 24
 
-	pwm = GPIO.PWM(pin, 1000)
-	GPIO.setup(pin_adelante, GPIO.OUT)
-	GPIO.setup(pin_atras, GPIO.OUT)
-
+GPIO.setup(pin_adelante, GPIO.OUT)
+GPIO.setup(pin_atras, GPIO.OUT)
+GPIO.setup(pin_pwm, GPIO.OUT)
+pwm = GPIO.PWM(pin_pwm, 1000)
+pwm.start(0)
 
 try: 
-	configuracion_pines();
 	GPIO.output(pin_adelante, GPIO.HIGH)
 	GPIO.output(pin_atras, GPIO.LOW)
 	while True:
+		print("DutyCycle: 75")
 		pwm.ChangeDutyCycle(75)
 		time.sleep(5)
+		print("DutyCycle: 50")
 		pwm.ChangeDutyCycle(50)
 		time.sleep(5)
+		print("DutyCycle: 45")
 		pwm.ChangeDutyCycle(45)
 		time.sleep(5)
-		pwm.ChangeDutyCycle(20)
+		print("DutyCycle: 25")
+		pwm.ChangeDutyCycle(25)
 		time.sleep(5)
+		
 except KeyboardInterrupt:
 	pwm.stop()
 	GPIO.cleanup()
