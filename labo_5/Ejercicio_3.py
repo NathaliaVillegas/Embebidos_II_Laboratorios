f72b2480db8e84ce413127e97b44a241ccc136fd
NathaliaVillegas <nathalia.villegas@ucb.edu.bo>
2026-03-26 15:59:07 -0400

    Primera versión ejrcicio3

---
+++ labo_5/Ejercicio_3.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+def configuracion_pines():
+	pin = 18
+	pin_adelante = 23
+	pin_atras = 24
+
+	pwm = GPIO.PWM(pin, 1000)
+	GPIO.setup(pin_adelante, GPIO.OUT)
+	GPIO.setup(pin_atras, GPIO.OUT)
+try: 
+	configuracion_pines();
+	while True:
+		pwm.ChangeDutyCycle(75)
+		time.sleep(5)
+		pwm.ChangeDutyCycle(50)
+		time.sleep(5)
+		pwm.ChangeDutyCycle(45)
+		time.sleep(5)
+		pwm.ChangeDutyCycle(20)
+		time.sleep(5)
+except KeyboardInterrupt:
+	pwm.stop()
+	GPIO.cleanup()
