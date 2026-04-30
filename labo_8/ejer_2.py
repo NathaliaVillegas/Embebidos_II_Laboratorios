391c98c96ddae7bd7a19d3cfc4d23910d8082f03
Nathalia <nathalia.villegas@ucb.edu.bo>
2026-04-30 19:08:04 -0400

    Cambios ejercicio 2

--- labo_8/ejer_2.py
+++ labo_8/ejer_2.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from abc import ABC, abstractmethod
 
+# Clase Abstracta según el estándar del laboratorio[cite: 1, 2]
 class CameraApp_abs(ABC):
     @abstractmethod
     def apply_filter(self, frame):
@@ -14,27 +15,37 @@ class CameraApp_abs(ABC):
 class FilterSystem(CameraApp_abs):
     def __init__(self):
         self.cap = cv2.VideoCapture(0)
-        self.filter_mode = '1' 
+        self.filter_mode = 'o' 
         
+        self.kernel_ridge = np.array([[-1, -1, -1], 
+                                      [-1,  8, -1], 
+                                      [-1, -1, -1]])
+        
+        self.kernel_sharpen = np.array([[ 0, -1,  0], 
+                                        [-1,  5, -1], 
+                                        [ 0, -1,  0]])
+        
+        self.kernel_blur = np.array([[1, 1, 1], 
+                                     [1, 1, 1], 
+                                     [1, 1, 1]]) / 9.0
+
     def apply_filter(self, frame):
+        frame_small = cv2.resize(frame, (400, 300))
         
         if self.filter_mode == '1':
-            return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            return cv2.filter2D(frame_small, -1, self.kernel_ridge)
             
         elif self.filter_mode == '2':
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            return cv2.Canny(gray, 50, 150)
+            return cv2.filter2D(frame_small, -1, self.kernel_sharpen)
             
         elif self.filter_mode == '3':
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-            return thresh
+            return cv2.filter2D(frame_small, -1, self.kernel_blur)
             
-        return frame 
+        return frame_small 
 
     def run(self):
-        print("Filtros")
-        print("Presiona: '1' Gris, '2' Bordes, '3' Binario, 'o' Original, 'Esc' Salir")
+        print("--- Sistema de Filtros por Kernels (Lab 8) ---")
+        print("Presiona: '1' Ridge, '2' Sharpen, '3' Blur, 'o' Original, 'Esc' Salir")
         
         while self.cap.isOpened():
             ret, frame = self.cap.read()
@@ -42,14 +53,14 @@ class FilterSystem(CameraApp_abs):
 
             processed_frame = self.apply_filter(frame)
             
-            cv2.imshow("Camara con Filtros - Julian", processed_frame)
+            cv2.imshow("Filtros Kernels - Julian", processed_frame)
             
             key = cv2.waitKey(1) & 0xFF
             
             if key == ord('1'): self.filter_mode = '1'
             elif key == ord('2'): self.filter_mode = '2'
             elif key == ord('3'): self.filter_mode = '3'
-            elif key == ord('o'): self.filter_mode = 'original'
+            elif key == ord('o'): self.filter_mode = 'o'
             elif key == 27: # Esc
                 break
                 
